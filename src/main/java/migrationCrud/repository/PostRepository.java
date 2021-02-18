b3e78f628c0a20cd0ae9eb9af7d5e409962f0327
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-02-18 14:32:57 +0100

    First commit

---
+++ src/main/java/migrationCrud/repository/PostRepository.java
@@ -0,0 +1,60 @@
+package migrationCrud.repository;
+
+import migrationCrud.exception.NotFoundException;
+import migrationCrud.model.Post;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Vector;
+
+
+@Repository
+public class PostRepository {
+    private final List<Post> allPosts;
+    private long idCounter;
+
+    public PostRepository() {
+        this.allPosts = new Vector<>();
+        idCounter = 0;
+    }
+
+    public List<Post> all() {
+        return allPosts;
+    }
+
+    public Optional<Post> getById(long id) {
+
+        for (Post post : allPosts) {
+            if (id == post.getId()) {
+                return Optional.of(post);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Post save(Post savePost) {
+        long idSavePost = savePost.getId();
+
+        if (idSavePost == 0) {
+            savePost.setId(++idCounter);
+            allPosts.add(savePost);
+            return savePost;
+        } else {
+            Optional<Post> newPost = getById(idSavePost);
+            if (newPost.isPresent()) {
+                Post post = newPost.get();
+                post.setContent(savePost.getContent());
+                return post;
+            } else {
+                throw new NotFoundException(String.format("Пост с ID = %d невозможно сохранить!", idSavePost));
+            }
+        }
+    }
+
+    public void removeById(long id) {
+        allPosts.removeIf(post -> id == post.getId());
+    }
+}
+
